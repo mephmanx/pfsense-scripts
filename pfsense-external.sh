8b3dbcd464fe216dcff19f8aca97e5600c003b8e
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2021-02-05 09:37:30 -0500

    Update pfsense-external.sh

--- pfsense-external.sh
+++ pfsense-external.sh
@@ -70,6 +70,6 @@ git commit -am "Auto Server Commit $(timestamp)"
 git push 
 
 }
-
-all_my_code
-
+{ all_my_code |
+  logger -p user.notice -t "$(basename "$0")"; } 2>&1 | 
+  logger -p user.error -t "$(basename "$0")"
