6a5d65074a70a33201376eff0acd57606fd5b240
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2021-02-05 08:50:08 -0500

    Create pfsense-external.sh

---
+++ pfsense-external.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+timestamp(){
+   date +"%d.%m.%Y um %H:%M"
+}
+
+all_my_code () {
+
+git pull
+rm -rf stamp.ver
+echo `jot -r 1 1 10000` > stamp.ver
+
+#### Add code here.  code outside is wrapper to detect things run
+
+
+
+
+##############
+
+git add *
+
+git commit -am "Auto Server Commit $(timestamp)"
+git push 
+
+}
+{ all_my_code |
+  logger -p user.notice -t "$(basename "$0")"; } 2>&1 | 
+  logger -p user.error -t "$(basename "$0")"
