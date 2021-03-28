68593189538aa62a82828785cf57ff20ca4e1a02
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2021-03-28 11:11:04 -0400

    Update pfsense-external.sh

--- pfsense-external.sh
+++ pfsense-external.sh
@@ -21,6 +21,8 @@ echo `jot -r 1 1 10000` > stamp.ver
 #### Add code here.  code outside is wrapper to detect things run
 IFS=
 
+####### Update cert on ESXi server
+
 scp /conf/acme/esxi.crt root@esxi.lyonsgroup.family:/tmp
 
 ssh -l root esxi.lyonsgroup.family -i ~/.ssh/id_rsa "
@@ -36,6 +38,28 @@ rm -rf /etc/vmware/ssl/rui.key.bk;
 mv /etc/vmware/ssl/rui.key /etc/vmware/ssl/rui.key.bk;
 mv /tmp/esxi.key /etc/vmware/ssl/rui.key;
 "
+
+###########################
+
+############# Update certs on Docker host
+
+scp /conf/acme/lyonsgroup-wildcard.crt root@cloudsupport.lyonsgroup.family:/tmp
+
+ssh -l root cloudsupport.lyonsgroup.family -i ~/.ssh/id_rsa "
+rm -rf /etc/vmware/ssl/rui.crt.bk;
+mv /etc/vmware/ssl/rui.crt /etc/vmware/ssl/rui.crt.bk;
+mv /tmp/esxi.crt /etc/vmware/ssl/rui.crt;
+"
+
+scp /conf/acme/lyonsgroup-wildcard.key root@cloudsupport.lyonsgroup.family:/tmp
+
+ssh -l root cloudsupport.lyonsgroup.family -i ~/.ssh/id_rsa "
+rm -rf /etc/vmware/ssl/rui.key.bk;
+mv /etc/vmware/ssl/rui.key /etc/vmware/ssl/rui.key.bk;
+mv /tmp/esxi.key /etc/vmware/ssl/rui.key;
+"
+
+############################
 
 ############################
 
